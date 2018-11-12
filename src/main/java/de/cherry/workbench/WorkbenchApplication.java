0456bf11ca58de8ac3044fca0ed205cd423dd116
max <baurmax1998@hotmail.com>
2018-11-12 18:08:39 +0100

    make runnable

--- src/main/java/de/cherry/workbench/WorkbenchApplication.java
+++ src/main/java/de/cherry/workbench/WorkbenchApplication.java
@@ -1,13 +1,10 @@
 package de.cherry.workbench;
 
-import de.cherry.workbench.sapui.model.Counter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkbenchApplication {
-
-	static Counter count = Counter.getInstance();
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkbenchApplication.class, args);
