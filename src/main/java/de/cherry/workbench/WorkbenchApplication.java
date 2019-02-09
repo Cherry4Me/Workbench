73b5486c402aa51c04c42489937dc2f623c6dabf
max <baurmax1998@hotmail.com>
2019-02-09 16:12:56 +0100

    system init

--- src/main/java/de/cherry/workbench/WorkbenchApplication.java
+++ src/main/java/de/cherry/workbench/WorkbenchApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WorkbenchApplication {
-
+	//hello world
 	public static void main(String[] args) {
 		SpringApplication.run(WorkbenchApplication.class, args);
 	}
